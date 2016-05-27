65b3bb3d3c3110133eb2bce3000d75ad2e0b67ce
cslzy <lizhenyang_2008@163.com>
2016-05-27 11:07:13 +0800

    send email demo

--- sendEmail/send_email_test.py
+++ sendEmail/send_email_test.py
@@ -1,11 +1,28 @@
 #! usr/bin/python
 #coding=utf-8
 # Author: cslzy
-# Email: lizhenyang@ruc.edu.cn
+# Email: lizhenyang_2008@163.com
 # Description: if the files number is enough, send email to me.
 # Date: 20151104 18:52:45
 from send_email import sendTo
 import os
 import time
 
-sendTo('lizhenyang_2008@163.com', 'the program is finished!')
+flag = True
+
+reciever_email = 'lizhenyang_2008@163.com' # you should change to you own
+sleepy_time = 3 * 3600
+print 'send message to %s every %d seconds'%(reciever_email, sleepy_time)
+
+i = 0
+while flag:
+    i += 1
+    print 'the %d\'th time to check'%i
+    message = ''
+    dirPath = '.'
+    if os.path.exists(dirPath):
+        listdir = os.listdir(dirPath)
+        message += 'there are %d file in %s\n'%(len(listdir), dirPath)
+    sendTo(reciever_email, message)
+    # sleep
+    time.sleep(sleepy_time)
